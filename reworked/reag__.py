9f09fd67f4717bdb19fd437f12495e7093201ad1
justuser31 <none>
2024-04-28 21:37:34 +0300

    up

---
+++ reworked/reag__.py
@@ -0,0 +1,8 @@
+class reag__:
+	def __init__(category = '-', comps, out):
+		# medicine
+		self.category = category
+		# {'инапровалин': 1, 'углерод': 1}
+		self.comps = comps
+		# 2
+		self.out = out
