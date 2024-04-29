61fba866009c028c5fa11d0b6ac9a46a00d33aaa
justuser31 <none>
2024-04-29 19:10:53 +0300

    Исправление всех ошибок, запуск: flask --app main run

--- reworked/frontend/main.py
+++ reworked/frontend/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def ss14tools():
-    render_template("./index.html")
+	return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
