0d3bd86b2c4c844006534293df8addeef79d5e4c
justuser31 <none>
2024-05-01 15:07:43 +0300

    Новый алгоритм, ура.

---
+++ reworked/calc.py
@@ -0,0 +1,36 @@
+def expand_recipe(recipe, recipes, main = False):
+	global expanded
+
+	ok = False
+	part = 1
+	while not ok:
+		ok = True
+		for el in recipe:
+			if el in recipes:
+				if part % recipes[el].out != 0:
+					ok = False
+					part += 1
+					expanded = {}
+					break
+				else:
+					expand_recipe(recipes[el].comps, recipes)
+			else:
+				if el in expanded:
+					expanded[el] += recipe[el]*part
+				else:
+					expanded[el] = recipe[el]*part
+
+def calc(element, amount, recipes):
+	# TODO: Пока только выводит расширенную версию рецепта
+	recipe, out = recipes[element].comps, recipes[element].out
+
+	global expanded
+	expanded = {}
+	expand_recipe(recipe, recipes, True)
+
+	return expanded
+
+
+from parse import *
+#print( load_recipes() )
+print( calc('Dylovene', 100, load_recipes()) )
